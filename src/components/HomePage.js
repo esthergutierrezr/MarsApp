49bfab8e045b15bb263126f82ca3fa39953b4650
sawnick <sawnick@DESKTOP-NMS566U.localdomain>
2020-11-19 18:35:27 +0000

    Added a map to click the planet

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,15 +1,24 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';
 import React from 'react';
+import {Link} from "react-router-dom";
 import "./Homepage.css";
+import Mars from "./images/mars_01.svg";
 
 
-{/* <map name="image-map">
-    <area target="" alt="" title="" href="./spaceship.js" coords="2256,2179,962" shape="circle">
-</map> */}
+
+const planet= (
+    <> <img src={Mars} alt='Planet' useMap="#image-map" />
+    <map name="image-map">
+        <Link to="/Spaceship"> <area className='planet' target="" alt="Planet" title=""  coords="2256,2179,962" shape="circle" /></Link>
+    </map> </>
+);
+
+
 
 function HomePage() {
     return (
         <div className="homepage">
+            {planet}
         </div>
     )
 }
