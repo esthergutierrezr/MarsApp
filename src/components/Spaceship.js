51a200442b4e950a9c0fd22ffaa47dd2af59da28
MortGoldman102 <danielgoldman102@gmail.com>
2020-11-19 13:51:56 +0000

    i have made changes to the route

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,18 +1,18 @@
 import React from "react";
 import Image from "./images/shuttle-cockpit-space-rocket.jpg";
 import Audio from "./images/Rocket countdown and blastoff.wav";
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
+import './Spaceship.css'
 
 function Spaceship() {
   return (
     <div>
-      <Link to="/land">
+      <Link to={"/spaceship/land"}>
         <img src={Image} className="cockpit" />
       </Link>
       <audio className="audio-element">
-          <source src={Audio}></source>
-        </audio>
+        <source src={Audio}></source>
+      </audio>
     </div>
   );
 }
