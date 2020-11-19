b6c5da2025e91e718f4c2e7498b049727640bfd2
HugoCravoMendes <hugo.cravomendes@gmail.com>
2020-11-19 20:23:17 +0000

    Info of API got, API_info changed to an array, API_info array mapped to the browser, info visible on browser

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
 
-function Weather() {
+class Weather extends React.Component {
+  state = {
+    weatherData: [],
+  };
+
+  componentDidMount() {
+    this.fetchWeather();
+    // console.log(weatherData);
+  }
+
+  fetchWeather = () => {
+    axios
+      .get(
+        "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0"
+      )
+      .then((response) => {
+        const { sol_keys, validity_checks, ...weatherData } = response.data;
+        const result = [];
+        for (let key in weatherData) {
+          result.push(weatherData[key]);
+        }
+        // console.log(result);
+        this.setState({ weatherData: result });
+        // console.log(response)
+      });
+  };
+
+  render() {
     return (
+      <div>
+        <p>hello</p>
+
         <div>
-            
+          {this.state.weatherData.map((data, index) => (
+            <h1 key={index}>{data.First_UTC}</h1>
+          ))}
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default Weather
-
+export default Weather;
