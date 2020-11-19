fc54cf350ab77c3b3e1fef8d3522b4df8b6693ab
sawnick <sawnick@DESKTOP-NMS566U.localdomain>
2020-11-19 15:05:53 +0000

    Added background image

---
+++ src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Homepage.css";
+import Mars from "./images/mars_01.svg";
+
+function HomePage() {
+    return (
+        <div className="homepage">
+            {/* <img src={Mars} alt=""/> */}
+        </div>
+    )
+}
+
+export default HomePage;
