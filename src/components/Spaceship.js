c72cff883d4652f36a8c8bcfeeac733413da81e9
MortGoldman102 <danielgoldman102@gmail.com>
2020-11-19 12:27:27 +0000

    i have added in the audio

--- src/components/Spaceship.js
+++ src/components/Spaceship.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "./images/shuttle-cockpit-space-rocket.jpg";
+import Audio from "./images/Rocket countdown and blastoff.wav";
 import { Link } from 'react-router-dom'
+
 
 function Spaceship() {
   return (
@@ -8,6 +10,9 @@ function Spaceship() {
       <Link to="/land">
         <img src={Image} className="cockpit" />
       </Link>
+      <audio className="audio-element">
+          <source src={Audio}></source>
+        </audio>
     </div>
   );
 }
